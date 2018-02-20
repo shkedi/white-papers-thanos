8ae9f3a81c58968ce5e2f644b5a9ca85b23c273f
Yair Shkedi <yair.shkedi@gmail.com>
2018-02-20 11:25:41 +0200

    change client id

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -5,7 +5,7 @@ interface AuthConfig {
 }
 
 export const AUTH_CONFIG: AuthConfig = {
-  clientID: 'YOUR_CLIENT_ID',
-  domain: 'YOUR_DOMAIN',
+  clientID: '_0PasBf4SgYqfjk_HDIs5EvYKo1PeYtu',
+  domain: 'fuseday2018.auth0.com',
   callbackURL: 'http://localhost:3000/callback'
 };
